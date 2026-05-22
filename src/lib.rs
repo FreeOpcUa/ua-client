b926cdc423944fe30b5ac54d8cd49b661e775488
Olivier <olivier@helitech>
2026-05-22 09:44:34 +0200

    make egui optional

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
 pub mod client;
 pub mod engine;
-pub mod eui;
 pub mod logger;
 pub mod messages;
 pub mod model;
 pub mod types;
+
+#[cfg(feature = "egui")]
+pub mod eui;
 
 #[cfg(feature = "tui")]
 pub mod tui;
